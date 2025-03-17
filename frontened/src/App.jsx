da1e82a0dc8a038d9d58a89863a5e0131e153549
mmiskatul <masabimiskat@gamil.com>
2025-03-17 14:23:47 +0600

    pages added

--- frontened/src/App.jsx
+++ frontened/src/App.jsx
@@ -1,14 +1,20 @@
 import  './App.css'
+import MovieCard from './MovieCard';
 
 function App() {
+  const movieNumber = 1;
+  
+ return(
   <>
-  <div>
-    <p>Hello World</p>
-  </div>
-  <div>
-    
-  </div>
+   {movieNumber===1?
+   (<MovieCard movie={{title:"tim's Film",release_date :"2024"}}/> )
+   :
+   (<MovieCard movie={{title:"joes Film",release_date :"2024"}}/>)
+   }
   </>
+ );
+
 }
+
 
 export default App
