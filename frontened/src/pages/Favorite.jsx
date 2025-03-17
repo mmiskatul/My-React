9b1b14eefa842f4f58515a6bd30059ccd04c146a
mmiskatul <masabimiskat@gamil.com>
2025-03-17 15:54:38 +0600

    the fontened

--- frontened/src/pages/Favorite.jsx
+++
@@ -1,7 +0,0 @@
-function Favorite (){
-    return <div className="favorite-empty">
-        <h2>No Favorite Movie yet</h2>
-        <p>Start adding movies to your favorite and they will appear here! </p>
-    </div>
-}
-export default Favorite
